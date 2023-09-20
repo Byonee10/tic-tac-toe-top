4e5265df4302d5356c79b1b9d17b83135a7eb6dd
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-09-21 02:26:19 +0300

    Added announce

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ const gameBoard = (() => {
     const getPosition = (row,column) => gameboard[row][column];
     const setPosition = (row,column,el) => gameboard[row][column] = el;
     const resetBoard = () => {
-        gameboard = [[0,0,0],
-        [0,0,0],
-        [0,0,0]];
+        gameboard = [["","",""],
+        ["","",""],
+        ["","",""]];
     };
     return{
     getBoard,
@@ -57,6 +57,7 @@ const gameController = (()=>{
         gameBoard.resetBoard();
         turn = true;
         board = gameBoard.getBoard();
+        winner = null;
     }
     const handleMove = (moveRow,moveColumn) => {
         if(!isGameOver()){
@@ -64,16 +65,21 @@ const gameController = (()=>{
             switchTurn();
         }
     }
-    const isGameOver = (board) =>
+    const isGameOver = () =>
     {
+        const board = gameBoard.getBoard();
         //check row and columns
         for(let i = 0; i<3; i++){
             if(board[i][0] !== "" && board[i][0] === board[i][1] && board[i][1]===board[i][2]){
-                winner == board[i][0];
+                winner = board[i][0];
+                return winner;
+
             }
 
             else if(board[0][i] !== "" && board[0][i] === board[1][i] && board[1][i] === board[2][i] ){
-                winner == board[0][i];
+                winner = board[0][i];
+                return winner;
+
             }
 
         }
@@ -82,15 +88,14 @@ const gameController = (()=>{
         
         if(board[0][0] !== "" && board[0][0]===board[1][1] && board[1][1] === board[2][2]){
             winner = board[0][0];
+            return winner;
+
         }
         else if(board[2][0]!=="" && board[2][0] === board[1][1]&& board[1][1] === board[0][2] ){
             winner = board[2][0];
-        }
-        
-        //if there is winner return the winner mark
-        if(winner){
             return winner;
         }
+        
         //if draw
         let isDraw = true;
 
@@ -131,7 +136,52 @@ function render(){
 
 };
 
+// Announce the winner or draw
+function announceResult(result) {
+    const message = result === 'draw' ? 'It\'s a draw!' : `${result} wins!`;
+    // Display the message in an alert or a div
+
+    setTimeout(() => {
+        alert(message);
+      }, 100);
+    }
+
+
+
+
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach((tile) => {
+    const row = tile.getAttribute("data-row");
+    const col = tile.getAttribute("data-col");
+});
+
+// Add this event listener to the "Restart" button to start a new game
 const restart = document.querySelector(".restart");
-restart.addEventListener("click",render);
+restart.addEventListener("click", () => {
+    gameBoard.resetBoard();
+    render();
 
+});
+// Add an event listener to each tile
+tiles.forEach((tile) => {
+  tile.addEventListener("click", () => {
+    // Get the row and column of the clicked tile
+    const row = tile.getAttribute("data-row");
+    const col = tile.getAttribute("data-col");
 
+    // Make a move on the game board
+
+    if(tile.innerHTML ==""){
+    gameController.handleMove(row, col);
+    }
+    // Update the rendering of the game board
+    render();
+
+    // Check if the game is over
+    const result = gameController.isGameOver();
+    if (result) {
+      // Announce the result of the game
+      announceResult(result);
+    }
+  });
+});
